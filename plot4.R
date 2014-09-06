c8f0c3b442e5a0ebbf2f42d931c905e111655012
ndhendrix <ndhendrix@gmail.com>
2014-09-06 09:42:45 -0700

    Comments and tidying up

--- plot4.R
+++ plot4.R
@@ -6,26 +6,36 @@ plot4 <- function() {
   data.sub <- data[year(data$Date2)=="2007",]
   data.sub <- data.sub[month(data.sub$Date2)==02,]
   data.sub <- data.sub[mday(data.sub$Date2) %in% c(1,2),]
+  
+  #removes huge dataframe
   rm(data)
+  
+  #casts Date and other variables into useable data types
   data.sub$Date <- strptime(paste(data.sub$Date, data.sub$Time), "%d/%m/%Y %H:%M:%S")
+  data.sub$Global_active_power <- as.numeric(as.character(data.sub$Global_active_power))
+  data.sub$Global_reactive_power <- as.numeric(as.character(data.sub$Global_reactive_power))
+  data.sub$Voltage <- as.numeric(as.character(data.sub$Voltage))
+  data.sub$Sub_metering_1 <- as.numeric(as.character(data.sub$Sub_metering_1))
+  data.sub$Sub_metering_2 <- as.numeric(as.character(data.sub$Sub_metering_2))
+  data.sub$Sub_metering_3 <- as.numeric(as.character(data.sub$Sub_metering_3))
   
   #creates graphic
   png("plot4.png")
   par(mfrow = c(2,2))
   
   #upper left plot
-  plot(data.sub$Date, as.numeric(as.character(data.sub$Global_active_power)), type="l", col="black", xlab=" ", ylab="Global Active Power")
+  plot(data.sub$Date, data.sub$Global_active_power, type="l", col="black", xlab=" ", ylab="Global Active Power")
   
   #upper right plot
-  plot(data.sub$Date, as.numeric(as.character(data.sub$Voltage)), type="l", col="black", xlab="datetime", ylab="Voltage")
+  plot(data.sub$Date, data.sub$Voltage, type="l", col="black", xlab="datetime", ylab="Voltage")
   
   #lower left plot
-  plot(data.sub$Date, as.numeric(as.character(data.sub$Sub_metering_1)), type="l", col="black", xlab=" ", ylab="Energy sub metering")
-  with(data.sub, lines(Date, as.numeric(as.character(Sub_metering_2)), col="red"))
-  with(data.sub, lines(Date, as.numeric(as.character(Sub_metering_3)), col="blue"))
+  plot(data.sub$Date, data.sub$Sub_metering_1, type="l", col="black", xlab=" ", ylab="Energy sub metering")
+  with(data.sub, lines(Date, Sub_metering_2, col="red"))
+  with(data.sub, lines(Date, Sub_metering_3, col="blue"))
   legend("topright", lwd="1", bty="n", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   
   #lower right plot
-  plot(data.sub$Date, as.numeric(as.character(data.sub$Global_reactive_power)), type="l", ylab="Global_reactive_power", xlab="datetime")
+  plot(data.sub$Date, data.sub$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
   dev.off()
 }
